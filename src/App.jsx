5b804c2f33d09086cf3e66e0db1a2120516d4d11
khadija <khadijanoorhashmi@gmail.com>
2023-06-14 23:13:27 +0500

    creating app file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import Pagination from './components/Pagination'
+import Search from './components/Search'
+import Stories from './components/Stories'
 
 
 function App() {
 
 
   return (
+    <>
+  <div>Welcome! to News Wesbsite</div>
+  <Search/>
+  <Pagination/>
+  <Stories/>
+    </>
     
-      <div style={{width: '100%', backgroundColor:"red"}}>hello</div>
   )
 }
 
