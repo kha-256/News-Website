9b23057a9c21953c0aef52d43e22a75273d9652a
khadija <khadijanoorhashmi@gmail.com>
2023-06-15 20:03:25 +0500

    fetching data from api

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 const Stories = () => {
+
+  let API = 'https://hn.algolia.com/api/v1/search?query=html'
+
+  const fetchDataApi = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data)
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  useEffect(() => {
+    fetchDataApi(API)
+  }, []);
+
   return (
-    <div>
-      Stories
-    </div>
+    <>
+      <h1>Welcome to Tech News Post</h1>
+    </>
   )
 }
 
