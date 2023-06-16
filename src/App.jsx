8087050204385bf76d8da207365c26b990b7f811
khadija <khadijanoorhashmi@gmail.com>
2023-06-16 22:59:07 +0500

    context api creation

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 import Pagination from './components/Pagination'
 import Search from './components/Search'
 import Stories from './components/Stories'
+import { useGlobalContext } from './Context'
 
 
 function App() {
 
+  const data= useGlobalContext()
 
   return (
     <>
-  <div>Welcome! to News Wesbsite</div>
+  <div>Welcome! to News Wesbsite by {data}</div>
   <Search/>
   <Pagination/>
   <Stories/>
