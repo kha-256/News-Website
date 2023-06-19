498648c96bb0143163f36c5d770363e076b61e8e
khadija <khadijanoorhashmi@gmail.com>
2023-06-19 23:02:41 +0500

    useReducer hook logic addition

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,12 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext, useReducer, useEffect } from "react";
+import reducer from "./reducer";
+
+let API = 'https://hn.algolia.com/api/v1/search?'
+
+
+const initialState={
+    isLoading:true,
+    query:"HTML",
+    nbPages: 0,
+    page:0,
+    hits:[]
+}
 
 const AppContext= React.createContext();
 
-// create proviser function
+// create provider function
 
 const AppProvider= ({children})=>{
 
-    return <AppContext.Provider value={'khadija'}>
+    const [state, dispatch]= useReducer(reducer, initialState);
+  const fetchDataApi = async (url) => {
+
+    dispatch({type:"SET_LOADING"})
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data)
+      dispatch({
+        type:"GET_STORIES",
+        payload:{
+            hits: data.hits,
+            nbPages: data.nbPages
+        }
+    })
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  useEffect(() => {
+    fetchDataApi(`${API}query=${state.query}&page=${state.page}`)
+  }, []);
+
+    return <AppContext.Provider value={{...state}}>
         {children}
     </AppContext.Provider>
 }
