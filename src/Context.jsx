3e27c909157fa16d78a937582623d52cfd30e657
khadija <khadijanoorhashmi@gmail.com>
2023-06-23 23:24:07 +0500

    stories page UI

--- src/Context.jsx
+++ src/Context.jsx
@@ -4,35 +4,35 @@ import reducer from "./reducer";
 let API = 'https://hn.algolia.com/api/v1/search?'
 
 
-const initialState={
-    isLoading:true,
-    query:"HTML",
-    nbPages: 0,
-    page:0,
-    hits:[]
+const initialState = {
+  isLoading: true,
+  query: "HTML",
+  nbPages: 0,
+  page: 0,
+  hits: []
 }
 
-const AppContext= React.createContext();
+const AppContext = React.createContext();
 
 // create provider function
 
-const AppProvider= ({children})=>{
+const AppProvider = ({ children }) => {
 
-    const [state, dispatch]= useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const fetchDataApi = async (url) => {
 
-    dispatch({type:"SET_LOADING"})
+    dispatch({ type: "SET_LOADING" })
     try {
       const res = await fetch(url);
       const data = await res.json();
       console.log(data)
       dispatch({
-        type:"GET_STORIES",
-        payload:{
-            hits: data.hits,
-            nbPages: data.nbPages
+        type: "GET_STORIES",
+        payload: {
+          hits: data.hits,
+          nbPages: data.nbPages
         }
-    })
+      })
     } catch (error) {
       console.log(error);
     }
@@ -43,13 +43,13 @@ const AppProvider= ({children})=>{
     fetchDataApi(`${API}query=${state.query}&page=${state.page}`)
   }, []);
 
-    return <AppContext.Provider value={{...state}}>
-        {children}
-    </AppContext.Provider>
+  return <AppContext.Provider value={{ ...state }}>
+    {children}
+  </AppContext.Provider>
 }
 
-const useGlobalContext=()=>{
-    return useContext(AppContext)
+const useGlobalContext = () => {
+  return useContext(AppContext)
 }
 
-export {AppContext, AppProvider, useGlobalContext}
+export { AppContext, AppProvider, useGlobalContext }
